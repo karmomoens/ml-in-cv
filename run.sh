966917f9d0c95a5a53fd6d8a543428284467d821
karmo <karel.moens@barco.com>
2018-07-10 10:25:58 +0200

    Automatically load pycharm when running

--- run.sh
+++ run.sh
@@ -1,15 +1,23 @@
+#!/bin/bash
+
+cmd="pycharm /media/local/ml-in-cv"
+if [ $1 != '' ]  
+then 
+  cmd=$1
+fi
+
 xhost +
 
 docker run -it --rm --privileged \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --network=host \
-    -v "$PWD:/media/local/student-trip-lyon" \
+    -v "$PWD:/media/local/ml-in-cv" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -v "$PWD/pyCharmConfig:/root/.PyCharmCE2017.1" \
     -v "$HOME/.Xauthority:/root/.Xauthority:rw" \
     -v /dev/video0:/dev/video0 \
     -v /dev/snd:/dev/snd \
-    bapha/student-trip-lyon bash
+    bapha/student-trip-lyon /bin/bash -c "$cmd"
 
 xhost -
