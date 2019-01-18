4d9a77bdb41c3ed54d636b73f2628454d7fe7eb2
karmo <karel.moens@barco.com>
2019-01-18 12:04:10 +0100

    Re-added and upgraded jupyter notebook support.

---
+++ run_jupyter.sh
@@ -0,0 +1,20 @@
+xhost +
+
+docker run -it --rm --privileged \
+    --env="DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --network=host \
+    -p 8888:8888 \
+    -p 8554:8554 \
+    -p 8555:8555 \
+    -p 8666:8666 \
+    -v "$PWD:/media/local/ml-in-cv" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+    -v "$PWD/pyCharmConfig:/root/.PyCharmCE2017.1" \
+    -v "$HOME/.Xauthority:/root/.Xauthority:rw" \
+    -v /dev/video0:/dev/video0 \
+    -v /dev/snd:/dev/snd \
+    -v $PWD/jupyter_notebook_config.py:/root/.jupyter/jupyter_notebook_config.py \
+    bapha/student-trip-lyon jupyter notebook
+
+xhost -
