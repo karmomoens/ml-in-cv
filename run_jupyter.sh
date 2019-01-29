4f2c8f1624df45583c4bfafc1f5ef0f60276e600
karmomoens <karel.moens@barco.com>
2019-01-29 15:37:43 +0100

    Some extra shared device, to foresee for depth channels.

--- run_jupyter.sh
+++ run_jupyter.sh
@@ -10,12 +10,13 @@ docker run -it --rm --privileged \
     -p 8666:8666 \
     -v "$PWD:/media/local/ml-in-cv" \
     -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
-    -v "$PWD/pyCharmConfig:/root/.PyCharmCE2017.1" \
     -v "$HOME/.Xauthority:/root/.Xauthority:rw" \
     -v /dev/video0:/dev/video0 \
     -v /dev/video1:/dev/video1 \
     -v /dev/video2:/dev/video2 \
     -v /dev/video3:/dev/video3 \
+    -v /dev/video4:/dev/video4 \
+    -v /dev/video5:/dev/video5 \
     -v /dev/snd:/dev/snd \
     -v $PWD/jupyter_notebook_config.py:/root/.jupyter/jupyter_notebook_config.py \
     bapha/student-trip-lyon jupyter notebook
